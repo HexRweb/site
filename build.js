9da4aff76902eaf365078e1584f9be6b6a751a74
HexR <hello@hexr.org>
2019-01-11 15:38:41 -0600

    migrate error pages to exstatic

--- build.js
+++ build.js
@@ -1,4 +1,9 @@
+const {promisify} = require('util');
+const {rename, rmdir} = require('fs');
 const Exstatic = require('exstatic');
+
+const move = promisify(rename);
+const del = promisify(rmdir);
 
 const dirs = {
     inputDir: './src/pages',
@@ -10,7 +15,14 @@ const exstatic = Exstatic(dirs);
 
 async function run() {
     await exstatic.initialize();
-    return exstatic.run();
+    await exstatic.loadFiles();
+    await exstatic.write();
+    // Exstatic is having issues handling explicit paths
+    await move('./built/error/index.html', './built/error.html');
+    await move('./built/error_403/index.html', './built/error_403.html');
+    await del('./built/error');
+    await del('./built/error_403');
+    exstatic.onBeforeExit(true);
 }
 
 run();

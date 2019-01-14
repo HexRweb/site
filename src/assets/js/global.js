f8d1febbb9a887942c59c005c09fd50d396c307e
HexR <hello@hexr.org>
2019-01-13 19:12:31 -0600

    prettify js assets

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	$(".button-collapse").sideNav();
-	$(".parallax").parallax();
+window.addEventListener('load', function() {
+	$('.button-collapse').sideNav();
+	$('.parallax').parallax();
 	$('.modal-trigger').leanModal();
 });
--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,1 +1,9 @@
-$(document).ready(function(){$(".project-coming-soon").hover(function(){$(this).find(".project-coming-soon-ribbon").fadeIn()},function(){$(this).find(".project-coming-soon-ribbon").hide()}),$(".project-logo").click(function(){null!==$(this).attr("data-bind-href")&&window.open($(this).attr("data-bind-href"))})});
+window.addEventListener('load', function () {
+	$('.project-coming-soon').hover(function () {
+		$(this).find('.project-coming-soon-ribbon').fadeIn()
+	}, function () {
+		$(this).find('.project-coming-soon-ribbon').hide()
+	}), $('.project-logo').click(function () {
+		null !== $(this).attr('data-bind-href') && window.open($(this).attr('data-bind-href'))
+	})
+});

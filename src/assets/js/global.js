c28aa6bfce7090a089229e22cde3c8a4f04da85f
HexR <hello@hexr.org>
2019-01-13 23:04:39 -0600

    fix modal init

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -2,6 +2,6 @@ window.addEventListener('load', function() {
 	window.hexr = {
 		sidenav: M.Sidenav.init(document.querySelectorAll('.sidenav')),
 		parallax: M.Parallax.init(document.querySelectorAll('.parallax')),
-		modals: M.Modal.init(document.querySelectorAll('.modal-trigger'))
+		modals: M.Modal.init(document.querySelectorAll('.modal'))
 	}
 });

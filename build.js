4a53d3a4650c30e722577e1b75afdf3d5078bbca
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-01-31 16:24:29 -0600

    Add exstatic build performance

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const STATIC_FILES = ['favicon.ico', 'robots.txt', 'sitemap.xml', '_redirects'];
 const exstatic = require('./@exstatic/packages/dev');
+const {performance} = require('perf_hooks');
 
 const instance = exstatic();
 
@@ -15,12 +16,23 @@ function copyFiles() {
 }
 
 async function compile() {
+	const start = performance.now();
 	await instance.build();
-}
-
-async function run() {
-	await Promise.all([compile(), copyFiles()]);
+	const time = performance.now() - start;
+	console.log(`Build took ${(time / 1000).toFixed(3)}s`);
 	instance.onBeforeExit(true);
 }
 
-run();
+async function run() {
+	console.log('Copying assets');
+	await copyFiles();
+	console.log('Building exstatic');
+	return compile();
+}
+
+if (process.argv.join(' ').match(/-?-exstatic/)) {
+	compile();
+} else {
+	run();
+}
+
